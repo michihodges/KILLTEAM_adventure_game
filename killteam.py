e4ae791d1c911b7b8723d1ee48a537ef58cf305e
michihodges <michihodges@gmail.com>
2020-10-18 23:19:12 +0100

    feat: Raven Guard
    Raven Guard first names added with random choice functionality printed.

--- killteam.py
+++ killteam.py
@@ -1,1 +1,7 @@
+# MODULES
 import random
+
+# RAVEN GUARD
+first_names = ['Aajz', 'Kyrin', 'Vykus', 'Tryris', 'Ordias', 'Navaer', 'Syras', 'Aevar', 'Reszan', 'Vorkyl']
+first_name = random.choice(first_names)
+print(first_name)
