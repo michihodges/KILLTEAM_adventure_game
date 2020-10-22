6adefab4396ddf44a5c5844bdb333abc42bfd640
michihodges <michihodges@gmail.com>
2020-10-22 21:16:23 +0100

    feat: Function
    Packaged code into function.

--- killteam.py
+++ killteam.py
@@ -2,8 +2,11 @@
 import random
 
 # RAVEN GUARD
-first_names = ['Aajz', 'Kyrin', 'Vykus', 'Tryris', 'Ordias', 'Navaer', 'Syras', 'Aevar', 'Reszan', 'Vorkyl']
-surnames = ['Kaed', 'Solari', 'Solaq', 'Qeld', 'Korvaedyn', 'Vannes', 'Torvaec', 'Klayde', 'Moradus', 'Ordaris']
-first_name = random.choice(first_names)
-surname = random.choice(surnames)
-print(first_name, surname)
+def name_generator():
+    first_names = ['Aajz', 'Kyrin', 'Vykus', 'Tryris', 'Ordias', 'Navaer', 'Syras', 'Aevar', 'Reszan', 'Vorkyl']
+    surnames = ['Kaed', 'Solari', 'Solaq', 'Qeld', 'Korvaedyn', 'Vannes', 'Torvaec', 'Klayde', 'Moradus', 'Ordaris']
+    first_name = random.choice(first_names)
+    surname = random.choice(surnames)
+    print(first_name, surname)
+
+name_generator()
