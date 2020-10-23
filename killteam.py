f137090bf63237b3a072015d6a3993da220d0fe9
michihodges <michihodges@gmail.com>
2020-10-23 23:15:48 +0100

    feat: For loop
    For loop added to make a batch of character names.

--- killteam.py
+++ killteam.py
@@ -2,11 +2,12 @@
 import random
 
 # RAVEN GUARD
-def name_generator():
+def name_generator(batch):
     first_names = ['Aajz', 'Kyrin', 'Vykus', 'Tryris', 'Ordias', 'Navaer', 'Syras', 'Aevar', 'Reszan', 'Vorkyl']
     surnames = ['Kaed', 'Solari', 'Solaq', 'Qeld', 'Korvaedyn', 'Vannes', 'Torvaec', 'Klayde', 'Moradus', 'Ordaris']
-    first_name = random.choice(first_names)
-    surname = random.choice(surnames)
-    print(first_name, surname)
+    for character in range(batch):
+        first_name = random.choice(first_names)
+        surname = random.choice(surnames)
+        print(first_name, surname)
 
-name_generator()
+name_generator(3)
