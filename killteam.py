d05bf8a7262f9a4284e23b1ff7c1589375f875d7
michihodges <michihodges@gmail.com>
2020-10-24 22:22:56 +0100

    feat: While loop
    Changed For loop to While loop, because While loops are the preferred loops for developing with in Python.

--- killteam.py
+++ killteam.py
@@ -5,9 +5,12 @@ import random
 def name_generator(batch):
     first_names = ['Aajz', 'Kyrin', 'Vykus', 'Tryris', 'Ordias', 'Navaer', 'Syras', 'Aevar', 'Reszan', 'Vorkyl']
     surnames = ['Kaed', 'Solari', 'Solaq', 'Qeld', 'Korvaedyn', 'Vannes', 'Torvaec', 'Klayde', 'Moradus', 'Ordaris']
-    for character in range(batch):
+
+    n = 0
+    while n < batch:
         first_name = random.choice(first_names)
         surname = random.choice(surnames)
         print(first_name, surname)
+        n += 1
 
 name_generator(3)
