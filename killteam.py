b95e3c6cd59f69b3068217d4d9530b2fbd9950e5
michihodges <michihodges@gmail.com>
2020-10-29 22:33:00 +0000

    feat: intro
    Intro added.

--- killteam.py
+++ killteam.py
@@ -7,6 +7,12 @@ def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(1)
 
+
+# INTRO
+def intro():
+    print_pause("Welcome to KILL TEAM Name Generator!")
+    print_pause("Pick your faction")
+
 # RAVEN GUARD
 def name_generator(batch):
     first_names = ['Aajz', 'Kyrin', 'Vykus', 'Tryris', 'Ordias', 'Navaer', 'Syras', 'Aevar', 'Reszan', 'Vorkyl']
@@ -19,4 +25,5 @@ def name_generator(batch):
         print(first_name, surname)
         n += 1
 
+intro()
 name_generator(3)
